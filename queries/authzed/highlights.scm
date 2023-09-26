d5ffe560c87c967d4c73dc3b4106455515d2a371
Mathew Polzin <matt.polzin@gmail.com>
2023-09-26 09:30:11 -0500

    wip updates to highlights file

--- queries/authzed/highlights.scm
+++ queries/authzed/highlights.scm
@@ -1,36 +1,61 @@
-((identifier) @keyword
- (#any-of? @keyword "definition" "permission" "relation"))
+((identifier) @function)
 
-(permission_literal) @variable.builtin
+(block
+  (relation
+    (relation_literal) @function.builtin
+    (identifier) @property))
 
-(permission (identifier) @type)
+(block
+  (permission
+    (permission_literal) @function.builtin
+    (identifier) @property))
+
+; relations
+((relation_literal) @function.builtin)
+; TODO: ask Mike about appearing to capture permission identifier
+;       and relation identifier as new names
 (relation (identifier) @constant)
-(perm_expression (identifier) @property)
-
-[
-  (block_start)
-  (block_end)
-] @punctuation.bracket
-
-(block (identifier) (identifier) @constructor)
-
-[
-  (plus_literal)
-  (pipe_literal)
-] @operator
-
-(relation_literal) @function
 (rel_expression (identifier) @property)
+
+((pipe_literal) @operator)
 
 (relation
   (rel_expression
     ((hash_literal)
-     . (identifier) @constant) @comment))
+     . (identifier) @constant)))
+
+; permissions
+; NOTE: asked Mike about second capture of permission_literal
+((permission_literal) @function.builtin)
+(permission (identifier) @type)
+(perm_expression (identifier) @property)
+
+((plus_literal) @operator)
+
+(call_expression
+  (selector_expression
+    (identifier) @constant))
+
+(call_expression
+  function: (selector_expression
+    field: (field_identifier) @function.method))
 
 (permission
   (perm_expression
-    ((stabby)
-     . (identifier) @function) @operator))
+    ((stabby) @operator
+     . (identifier) @function)))
+
+; misc
+[
+  (true)
+  (false)
+  (nil)
+] @constant.builtin
+
+[
+ (caveat_literal)
+ (definition_literal)
+] @keyword
 
 [
   (hash_literal)
